62322eff2f75c3ee76318346b43f48945e99f18e
anil2743 <nakkaanilyadav2743@gmail.com>
2025-06-17 19:47:35 +0530

    pom.xml changed

--- src/main/java/com/servlet/register/RegisterServlet.java
+++ src/main/java/com/servlet/register/RegisterServlet.java
@@ -8,6 +8,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
